49ab9932f7afe4ef8b587538011bedbd7df291d6
Aarinturf5 <naaron003@gmail.com>
2022-08-18 21:31:11 +0200

    My first Commit

---
+++ bursar/index.php
@@ -0,0 +1,130 @@
+
+<?php
+session_start();
+
+if(!isset($_SESSION['b_lname'])){
+	
+	echo "<script>window.open('login.php?not_authorize=<h3>Authentication failure! Please Contact System Admin.</h3>','_self')</script>";
+}
+
+else {
+
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<title>Bursar</title>
+<link rel="stylesheet"  href="style.css" />
+</head>
+
+<body>
+
+<div class="wrapper">
+	
+	<!--This is header-->
+	<div class="header" align="center"><h1>Supervisor</h1></div>
+	<!--End of header-->
+	
+	
+	<!--This is left sidebar-->
+	<div class="left">
+	<br>
+		<h3 style="padding: 10px; text-decoration: underline; font-weight: bold;">MANAGEMENT CONTENT:</h3>
+	<br>
+		
+		<button class="button" style="vertical-align: middle"><span><a href="index.php?view_all_customers">VIEW CUSTOMERS</a></span></button>
+		<button class="button" style="vertical-align: middle"><span><a href="index.php?#">VEHICLE PROGRESS</a></span></button>
+		<button class="button" style="vertical-align: middle"><span><a href="index.php?new_invoice">NEW INVOICE</a></span></button>
+		 <button class="button" style="vertical-align: middle"><span><a href="index.php?view_all_invoice">VIEW INVOICE</a></span></button>
+		  <button class="button" style="vertical-align: middle"><span><a href="index.php?view_report">VIEW REPORT</a></span></button>
+		    <button class="button" style="vertical-align: middle"><span><a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">LOGOUT</a></span></button>
+		
+	</div>
+	<!--End of left sidebar-->
+	
+	
+	<!--This is Content-->
+	<div class="right">
+	
+	<h3 style="color: #16365d; text-align: right;"> Hello  <?php echo ucwords($_SESSION['b_lname']); ?>! </h3>
+	
+	
+		<?php
+		
+          if(isset($_GET['view_all_customers'])){
+			  
+	      include("includes/view_all_customers.php");
+			  
+          }
+	  
+	      if(isset($_GET['edit_customer'])){
+			  
+	      include("includes/edit_customer.php");
+			  
+          }
+	  
+	      if(isset($_GET['new_invoice'])){
+			  
+	      include("includes/new_invoice.php");
+			  
+          }
+	  
+	      if(isset($_GET['edit_invoice'])){
+			  
+	      include("includes/edit_invoice.php");
+			  
+          }
+	  
+	      if(isset($_GET['view_all_invoice'])){
+			  
+	      include("includes/view_all_invoice.php");
+			  
+          }
+	  
+	      if(isset($_GET['view_report'])){
+			  
+	      include("includes/view_report.php");
+			  
+          }
+	  
+	      if(isset($_GET['delete_invoice'])){
+			  
+	      include("includes/delete_invoice.php");
+			  
+          }
+	
+	
+	
+          
+
+		
+         ?>
+         
+	</div>
+	<!--End of Content-->
+
+	<!--This is Footer-->
+	<div align="center" style="background-color:#16365d; font-size: 20px; color: #fff;" >
+	<i>© 2022 ALL RIGHTS RESERVED – Aaron </i>
+	<h6>(GARAGE MANAGEMENT SYSTEM)</h6>
+	<h6><i>Contact Us: +25761552799 / ndiaar@biu.bi<i></h6>
+	</div>
+	<!--End of Footer-->
+				
+</div>
+
+
+</body>
+
+	
+</html>
+
+
+<?php } ?>
+
+
---
+++ index.php
@@ -0,0 +1,57 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Welcome</title>
+<link rel="stylesheet"  href="welcome_style.css" />
+</head>
+
+<body>
+
+<div class="wrapper">
+	
+	<!--This is header-->
+	<div class="header" align="center"><h1>GARAGE MANAGEMENT SYSTEM</h2></div>
+	<!--End of header-->
+	
+	
+	<!--This is content-->
+	<div class="content">
+	<br>
+		<h2 style="padding: 10px; text-decoration: underline; font-weight: bold;">PLEASE CHOOSE YOUR FIELD BELOW TO CONTINUE:</h2>
+	<br>
+		 
+		 
+		 <button class="button2" style="vertical-align: middle" onclick="window.location.href='http://localhost/GMS/receiption/login.php'" ><h2>RECEPTIONIST</h2></button>
+		 
+		 <button class="button2" style="vertical-align: middle" onclick="window.location.href='http://localhost/GMS/bursar/login.php'" ><h2>SUPERVISOR</h2></button>
+		 
+		 <button class="button2" style="vertical-align: middle" onclick="window.location.href='http://localhost/GMS/admin/login.php'" ><h2>ADMIN</h2></button>
+		    	
+	</div>
+	<!--End of content-->
+	
+
+	<!--This is Footer-->
+	<div align="center" style="background-color:#16365d; font-size: 20px; color: #fff;" >
+	<i>© 2022 ALL RIGHTS RESERVED – Aaron </i>
+	<h6>(GARAGE MANAGEMENT SYSTEM)</h6>
+	<h6><i>Contact Us: +25761552799 / ndiaar@biu.bi<i></h6>
+	</div>
+	<!--End of Footer-->
+				
+</div>
+
+
+
+</body>
+
+
+	
+</html>
+
+
+
+
+
